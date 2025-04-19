f1e459fadffad5d6dee2bff7e68d9179b25e9771
Harish patel <hkp@gmail.com>
2025-04-20 01:39:26 +0530

    upadated addtocarttest

--- src/test/java/testcases/AddToCartTest.java
+++ src/test/java/testcases/AddToCartTest.java
@@ -1,12 +1,7 @@
 package testcases;
 
 import java.time.Duration;
-import java.util.List;
-import java.util.Set;
-
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
